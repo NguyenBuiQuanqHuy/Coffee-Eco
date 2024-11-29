79e500d41ebc03411d33ee17e52f1d9598db38eb
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-11-29 15:52:25 +0700

    Updated

--- Coffee_Eco/src/main/java/huy/ntu/coffee_eco/Controller/LoginController.java
+++ Coffee_Eco/src/main/java/huy/ntu/coffee_eco/Controller/LoginController.java
@@ -1,8 +1,5 @@
 package huy.ntu.coffee_eco.Controller;
 public class LoginController {
-
-
-
     public void Login(){
 
     }
---
+++ Coffee_Eco/src/main/java/huy/ntu/coffee_eco/Controller/MenuController.java
@@ -0,0 +1,35 @@
+package huy.ntu.coffee_eco.Controller;
+
+import huy.ntu.coffee_eco.Models.Entities.NhanVien;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class MenuController {
+    public void handleAddSP(){
+
+    }
+
+    public void minusSoluong(){
+
+    }
+
+    public void plusSoluong(){
+        
+    }
+
+    public void handlePay(){
+
+    }
+
+    public void ThongKe(){
+
+    }
+
+    public void QuanLyMenu(){
+
+    }
+
+    public void QuanLyNhanVien(){
+
+    }
+}
